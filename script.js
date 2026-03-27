830f4b3e1d0f99c5ed356288156944da8838cf20
ItsStunZ <115426921+ItsStunZ@users.noreply.github.com>
2026-03-27 00:33:15 +0000

    Started working on checkForWinner Logic

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const Gameboard = (() => {
-  let gameBoard = ['', '', '',
-                   '', '', '',
-                   '', '', ''];
+  let gameBoard = ['O', 'X', 'O',
+                   'X', 'O', 'X',
+                   'X', 'O', 'X'];
 
   const updateBoard = (pos, player) => {
     gameBoard[pos] = player;
-    console.log(gameBoard);
   }
   
-  return { updateBoard };
+  const checkForWinner = () => {
+    // Horizontal
+    if (new Set([gameBoard[0], gameBoard[1], gameBoard[2]]).size === 1 || 
+        new Set([gameBoard[3], gameBoard[4], gameBoard[5]]).size === 1 || 
+        new Set([gameBoard[6], gameBoard[7], gameBoard[8]]).size === 1)
+        return true;
+
+    // Vertical
+    if (new Set([gameBoard[0], gameBoard[3], gameBoard[6]]).size === 1 ||
+        new Set([gameBoard[1], gameBoard[4], gameBoard[7]]).size === 1 ||
+        new Set([gameBoard[2], gameBoard[5], gameBoard[8]]).size === 1)
+        return true;
+
+    // TODO: Add horizontal check
+    return false;
+  }
+
+  const getGameboard = () => gameBoard();
+  
+  return { updateBoard, checkForWinner, getGameboard};
 })();
 
 const Players = (() => {
@@ -25,9 +43,16 @@ const GameManager = (() => {
   const start = () => {
     // loop until some checks are valid (matching 3 or tie)
     do { 
-      let input = parseInt(prompt(`Player ${_currentPlayerTurn} turn: `));
+    //   let input = parseInt(prompt(`Player ${_currentPlayerTurn} turn: `));
       // update gameboard
-      Gameboard.updateBoard(input, _currentPlayerTurn);
+    //   Gameboard.updateBoard(input, _currentPlayerTurn);
+      // check for winner
+      const isWinner = Gameboard.checkForWinner();
+      if (isWinner) {
+        winner = 'a';
+        alert('Winner');
+        console.log(GameManager.getGameBoard)
+      }
       // update _currentPlayerTurn
       _currentPlayerTurn = _currentPlayerTurn == Players.playerX ? Players.playerO : Players.playerX;
       console.log(`Next player is ${_currentPlayerTurn}`);
@@ -36,3 +61,5 @@ const GameManager = (() => {
 
   return { start };
 })();
+
+GameManager.start();
